128a3a0e7f30e9d2b60331985d4910ac7786cd3e
Henry Lin <henry.lin@appdirect.com>
2018-12-24 08:15:17 -0800

    Advanced Lists

---
+++ Advanced_Lists.py
@@ -0,0 +1,15 @@
+In this series of lectures we will be diving a little deeper into all the methods available in a list object. These aren't officially "advanced" features, just methods that you wouldn't typically encounter without some additional exploring. Its pretty likely that you've already encountered some of these yourself!
+
+Lets begin!
+
+In [1]:
+l = [1,2,3]
+append
+You will have definitely have use this method by now, which merely appends an element to the end of a list:
+
+In [2]:
+l.append(4)
+
+l
+Out[2]:
+[1, 2, 3, 4]
