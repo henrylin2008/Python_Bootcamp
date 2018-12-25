5871f7f7b26c44d8bb031d29b8d37a057422cafe
Henry Lin <henry.lin@appdirect.com>
2018-12-25 07:05:12 -0800

    AN -v2

--- Advanced_Numbers.py
+++ Advanced_Numbers.py
@@ -9,3 +9,20 @@ In [5]:
 hex(512)
 Out[5]:
 '0x200'
+
+
+Binary
+Using the function bin() you can convert numbers into their binary format.
+
+In [19]:
+bin(1234)
+Out[19]:
+'0b10011010010'
+In [18]:
+bin(128)
+Out[18]:
+'0b10000000'
+In [16]:
+bin(512)
+Out[16]:
+'0b1000000000'
