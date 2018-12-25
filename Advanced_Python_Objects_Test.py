4469b7a2262be1adf74d77900e85b6688446f7e0
Henry Lin <henry.lin@appdirect.com>
2018-12-25 07:08:05 -0800

    APOT v3

--- Advanced_Python_Objects_Test.py
+++ Advanced_Python_Objects_Test.py
@@ -13,3 +13,13 @@ In [5]:
 round(5.23222,2)
 Out[5]:
 5.23
+
+Advanced Strings
+Problem 3: Check if every letter in the string s is lower case
+
+In [7]:
+s = 'hello how are you Mary, are you feeling okay?'
+
+s.islower()
+Out[7]:
+False
