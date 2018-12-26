136ec7b41a0a7a7bed028ea51cbe7e1a34f2c334
Henry Lin <henry.lin@appdirect.com>
2018-12-26 06:09:45 -0800

    AS

---
+++ Advanced_Sets.py
@@ -0,0 +1,15 @@
+In this lecture we will learn about the various methods for sets that you may not have seen yet. We'll go over the basic ones you already know and then dive a little deeper.
+
+In [2]:
+s = set()
+add
+add elements to a set. Remember a set won't take duplicate elements and only present them once (thats why its called a set!)
+
+In [3]:
+s.add(1)
+In [4]:
+s.add(2)
+In [5]:
+s
+Out[5]:
+{1, 2}
