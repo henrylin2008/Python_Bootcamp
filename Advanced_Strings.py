49b002a13f7bf96fe5f7c527d3f6bd372affd657
Henry Lin <henry.lin@appdirect.com>
2018-12-27 07:49:59 -0800

    AS v2

--- Advanced_Strings.py
+++ Advanced_Strings.py
@@ -2,3 +2,21 @@ String objects have a variety of methods we can use to save time and add functio
 
 In [75]:
 s = 'hello world'
+
+
+Changing case
+We can use methods to capitalize the first word of a string, change cases to upper and lower case strings.
+
+In [76]:
+# Capitalize first word in string
+s.capitalize()
+Out[76]:
+'Hello world'
+In [77]:
+s.upper()
+Out[77]:
+'HELLO WORLD'
+In [78]:
+s.lower()
+Out[78]:
+'hello world'
