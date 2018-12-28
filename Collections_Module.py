51cae8878aa16a647192a3e04dfbf0e63358e4ee
Henry Lin <henry.lin@appdirect.com>
2018-12-28 06:14:44 -0800

    collections module

---
+++ Collections_Module.py
@@ -0,0 +1,3 @@
+The collections module is a built-in module that implements specialized container data types providing alternatives to Python’s general purpose built-in containers. We've already gone over the basics: dict, list, set, and tuple.
+
+Now we'll learn about the alternatives that the collections module provides.
