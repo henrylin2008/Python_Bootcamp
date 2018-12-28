92a09ca4d390a8016f31e1bea76f099e30645f46
Henry Lin <henry.lin@appdirect.com>
2018-12-28 06:12:37 -0800

    Chained CO

---
+++ Chained_Comparison_Operators.py
@@ -0,0 +1,10 @@
+An interesting feature of Python is the ability to chain multiple comparisons to perform a more complex test. You can use these chained comparisons as a shorthand for larger Boolean Expressions.
+
+In this lecture we will learn how to chain comparison operators and we will also introduce two other important statements in python: and and or.
+
+Let's look at a few examples of using chains:
+
+In [1]:
+1 < 2 < 3
+Out[1]:
+True
