2fbd655dbee86f18a563e6985c4109216f227c69
Henry Lin <henry.lin@appdirect.com>
2018-12-28 06:15:41 -0800

    collections module

--- Collections_Module.py
+++ Collections_Module.py
@@ -25,3 +25,21 @@ In [3]:
 Counter('aabsbsbsbhshhbbsbs')
 Out[3]:
 Counter({'b': 7, 's': 6, 'h': 3, 'a': 2})
+
+Counter with words in a sentence
+
+In [4]:
+s = 'How many times does each word show up in this sentence word times each each word'
+
+words = s.split()
+
+Counter(words)
+Out[4]:
+Counter({'word': 3, 'each': 3, 'times': 2, 'show': 1, 'this': 1, 'many': 1, 'in': 1, 'up': 1, 'How': 1, 'does': 1, 'sentence': 1})
+In [5]:
+# Methods with Counter()
+c = Counter(words)
+
+c.most_common(2)
+Out[5]:
+[('word', 3), ('each', 3)]
