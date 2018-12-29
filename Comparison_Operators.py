33e691e2b6cc33760d2464871e557e45fd82def9
Henry Lin <henry.lin@appdirect.com>
2018-12-29 07:59:19 -0800

    Comparsion Operator

--- Comparison_Operators.py
+++ Comparison_Operators.py
@@ -7,3 +7,13 @@ Operator	Description	Example
 <	If the value of left operand is less than the value of right operand, then condition becomes true.	(a < b) is true.
 >=	If the value of left operand is greater than or equal to the value of right operand, then condition becomes true.	(a >= b) is not true.
 <=	If the value of left operand is less than or equal to the value of right operand, then condition becomes true.	(a <= b) is true.
+
+Equal¶
+In [3]:
+2 == 2
+Out[3]:
+True
+In [4]:
+1 == 0
+Out[4]:
+False
