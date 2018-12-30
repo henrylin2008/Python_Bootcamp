89fb79bdef52550861b5c8d4e1c0c4e9ffcf5771
Henry Lin <henry.lin@appdirect.com>
2018-12-30 07:59:59 -0800

    Decorators

---
+++ Decorators.py
@@ -0,0 +1,13 @@
+Decorators
+Decorators can be thought of as functions which modify the functionality of another function. They help to make your code shorter and more "Pythonic".
+
+To properly explain decorators we will slowly build up from functions. Make sure to restart the Python and the Notebooks for this lecture to look the same on your own computer. So lets break down the steps:
+
+Functions Review
+In [1]:
+def func():
+    return 1
+In [2]:
+func()
+Out[2]:
+1
