4c18a523de4f1d805c448274f0237bb5253c02e3
Henry Lin <henry.lin@appdirect.com>
2018-12-31 05:45:50 -0800

    Dictionaries

--- Dictionaries.py
+++ Dictionaries.py
@@ -11,3 +11,15 @@ So what are mappings? Mappings are a collection of objects that are stored by a 
 
 A Python dictionary consists of a key and then an associated value. That value can be almost any Python object.
 
+
+Constructing a Dictionary
+Let's see how we can construct dictionaries to get a better understanding of how they work!
+
+In [1]:
+# Make a dictionary with {} and : to signify a key and a value
+my_dict = {'key1':'value1','key2':'value2'}
+In [2]:
+# Call values by their key
+my_dict['key2']
+Out[2]:
+'value2'
