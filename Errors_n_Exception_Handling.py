92ba436b60cec94dc3f09970a5236e1da9b3f657
Henry Lin <henry.lin@appdirect.com>
2019-01-02 06:29:38 -0800

    E&EH

--- Errors_n_Exception_Handling.py
+++ Errors_n_Exception_Handling.py
@@ -236,3 +236,27 @@ finally:
     print 'All Done!'
 Can't divide by Zero!
 All Done!
+
+Problem 3
+Write a function that asks for an integer and prints the square of it. Use a while loop with a try,except, else block to account for incorrect inputs.
+
+In [24]:
+def ask():
+    
+    while True:
+        try:
+            n = input('Input an integer: ')
+        except:
+            print 'An error occurred! Please try again!'
+            continue
+        else:
+            break
+            
+        
+    print 'Thank you, you number squared is: ',n**2
+In [25]:
+ask()
+Input an integer: null
+An error occurred! Please try again!
+Input an integer: 2
+Thank you, you number squared is:  4
