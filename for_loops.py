c69ca4e000ee2b42153f050f799751bcb65330ba
Henry Lin <henry.lin@appdirect.com>
2019-01-03 16:02:30 -0800

    FL

---
+++ for_loops.py
@@ -0,0 +1,9 @@
+for Loops
+A for loop acts as an iterator in Python, it goes through items that are in a sequence or any other iterable item. Objects that we've learned about that we can iterate over include strings,lists,tuples, and even built in iterables for dictionaries, such as the keys or values.
+
+We've already seen the for statement a little bit in past lectures but now lets formalize our understanding.
+
+Here's the general format for a for loop in Python:
+
+for item in object:
+    statements to do stuff
