30075354ec11ed5a3c5de08ab46122a3593a946e
Henry Lin <henry.lin@appdirect.com>
2019-01-05 07:14:33 -0800

    Functions

--- Functions.py
+++ Functions.py
@@ -47,3 +47,50 @@ result = add_num(4,5)
 In [11]:
 print result
 9
+
+What happens if we input two strings?
+
+In [12]:
+print add_num('one','two')
+onetwo
+Note that because we don't declare variable types in Python, this function could be used to add numbers or sequences together! We'll later learn about adding in checks to make sure a user puts in the correct arguments into a function.
+
+Lets also start using break,continue, and pass statements in our code. We introduced these during the while lecture.
+
+Finally lets go over a full example of creating a function to check if a number is prime ( a common interview exercise).
+
+We know a number is prime if that number is only evenly divisible by 1 and itself. Let's write our first version of the function to check all the numbers from 1 to N and perform modulo checks.
+
+In [8]:
+def is_prime(num):
+    '''
+    Naive method of checking for primes. 
+    '''
+    for n in range(2,num):
+        if num % n == 0:
+            print 'not prime'
+            break
+    else: # If never mod zero, then prime
+        print 'prime'
+In [9]:
+is_prime(16)
+not prime
+Note how we break the code after the print statement! We can actually improve this by only checking to the square root of the target number, also we can disregard all even numbers after checking for 2. We'll also switch to returning a boolean value to get an example of using return statements:
+
+In [15]:
+import math
+
+def is_prime(num):
+    '''
+    Better method of checking for primes. 
+    '''
+    if num % 2 == 0 and num > 2: 
+        return False
+    for i in range(3, int(math.sqrt(num)) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
+In [16]:
+is_prime(14)
+Out[16]:
+False
