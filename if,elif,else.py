6973dfae096e27fb01b5304c02a7f6349b11cbff
Henry Lin <henry.lin@appdirect.com>
2019-01-07 06:50:27 -0800

    If elif else

--- if,elif,else.py
+++ if,elif,else.py
@@ -34,3 +34,22 @@ if x:
 else:
     print 'I will be printed in any case where x is not true'
 I will be printed in any case where x is not true
+
+
+Multiple Branches
+Let's get a fuller picture of how far if, elif, and else can take us!
+
+We write this out in a nested structure. Take note of how the if,elif,and else line up in the code. This can help you see what if is related to what elif or else statements.
+
+We'll reintroduce a comparison syntax for Python.
+
+In [6]:
+loc = 'Bank'
+
+if loc == 'Auto Shop':
+    print 'Welcome to the Auto Shop!'
+elif loc == 'Bank':
+    print 'Welcome to the bank!'
+else:
+    print "Where are you?"
+Welcome to the bank!
