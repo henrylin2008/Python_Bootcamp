11c4f3955c9afe8f613fde40e1e0a15dd629696a
Henry Lin <henry.lin@appdirect.com>
2019-01-07 06:52:03 -0800

    Python S

---
+++ Python_Statements.py
@@ -0,0 +1,6 @@
+In this lecture we will be doing a quick overview of Python Statements. This lecture will emphasize differences between Python and other languages such as C++.
+
+There are two reasons we take this approach for learning the context of Python Statements:
+
+1.) If you are coming from a different language this will rapidly accelerate your understanding of Python.
+2.) Learning about statements will allow you to be able to read other languages more easily in the future.
