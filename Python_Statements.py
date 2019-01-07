e7f33563f3a9f32ac6bda8574adad6846bd30875
Henry Lin <henry.lin@appdirect.com>
2019-01-07 06:52:13 -0800

    Python S

--- Python_Statements.py
+++ Python_Statements.py
@@ -4,3 +4,21 @@ There are two reasons we take this approach for learning the context of Python S
 
 1.) If you are coming from a different language this will rapidly accelerate your understanding of Python.
 2.) Learning about statements will allow you to be able to read other languages more easily in the future.
+
+Python vs Other Languages
+Let's create a simple statement that says: "If a is greater than b, assign 2 to a and 4 to b"
+
+Take a look at these two if statements (we will learn about building out if statements soon).
+
+Version 1 (Other Languages)
+
+if (a>b){
+    a = 2;
+    b = 4;
+}
+
+Version 2 (Python)
+
+if a>b:
+    a = 2
+    b = 4
