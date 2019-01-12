af3e4ac8dabea27329cdb3f14fff6cc116565395
Henry Lin <henry.lin@appdirect.com>
2019-01-11 20:20:20 -0800

    Lists

--- lists.py
+++ lists.py
@@ -8,3 +8,21 @@ In this section we will learn about:
 3.) Basic List Methods
 4.) Nesting Lists
 5.) Introduction to List Comprehensions
+
+Lists are constructed with brackets [] and commas separating every element in the list.
+
+Let's go ahead and see how we can construct lists!
+
+In [2]:
+# Assign a list to an variable named my_list
+my_list = [1,2,3]
+We just created a list of integers, but lists can actually hold different object types. For example:
+
+In [4]:
+my_list = ['A string',23,100.232,'o']
+Just like strings, the len() function will tell you how many items are in the sequence of the list.
+
+In [6]:
+len(my_list)
+Out[6]:
+4
