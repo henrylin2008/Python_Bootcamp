dcb5994f6f16a9fc9a3833cd12bbcb9e4cc67470
Henry Lin <henry.lin@appdirect.com>
2019-01-13 20:25:40 -0800

    Methods

--- methods.py
+++ methods.py
@@ -33,3 +33,21 @@ In [4]:
 l
 Out[4]:
 [1, 2, 3, 4, 5, 6]
+
+Great! Now how about count()? The count() method will count the number of occurrences of an element in a list.
+
+In [7]:
+# Check how many times 2 shows up in the list
+l.count(2)
+Out[7]:
+1
+You can always use Shift+Tab in the Jupyter Notebook to get more help about the method. In general Python you can use the help() function:
+
+In [17]:
+help(l.count)
+Help on built-in function count:
+
+count(...)
+    L.count(value) -> integer -- return number of occurrences of value
+
+Feel free to play around with the rest of the methods for a list. Later on in this section your quiz will involve using help and Google searching for methods of different types of objects!
