2971dac65792ef7abf48b5575821401d21cd09c4
Henry Lin <henry.lin@appdirect.com>
2019-01-13 20:24:41 -0800

    Methods

---
+++ methods.py
@@ -0,0 +1,8 @@
+Methods
+We've already seen a few example of methods when learning about Object and Data Structure Types in Python. Methods are essentially functions built into objects. Later on in the course we will learn about how to create our own objects and methods using Object Oriented Programming (OOP) and classes.
+
+Methods will perform specific actions on the object and can also take arguments, just like a function. This lecture will serve as just a brief introduction to methods and get you thinking about overall design methods that we will touch back upon when we reach OOP in the course.
+
+Methods are in the form:
+
+object.method(arg1,arg2,etc...)
