0a3e0bd70d7a125fd49b6dbd24dce84863e9dc42
Henry Lin <henry.lin@appdirect.com>
2019-01-14 21:52:25 -0800

    NSS

--- Nested_Statements_&_Scope.py
+++ Nested_Statements_&_Scope.py
@@ -35,3 +35,18 @@ built-in
 Names declared in global and nonlocal statements map assigned names to enclosing module and function scopes.
 The statement in #2 above can be defined by the LEGB rule.
 
+LEGB Rule.
+
+L: Local — Names assigned in any way within a function (def or lambda)), and not declared global in that function.
+
+E: Enclosing function locals — Name in the local scope of any and all enclosing functions (def or lambda), from inner to outer.
+
+G: Global (module) — Names assigned at the top-level of a module file, or declared global in a def within the file.
+
+B: Built-in (Python) — Names preassigned in the built-in names module : open,range,SyntaxError,...
+
+Quick examples of LEGB
+Local
+In [15]:
+# x is local here:
+f = lambda x:x**2
