e02553dd44d857b508d9c823bc2cfb22d172794a
Henry Lin <henry.lin@appdirect.com>
2019-01-14 21:40:10 -0800

    MP

--- Modules_Packages.py
+++ Modules_Packages.py
@@ -72,3 +72,15 @@ ceil(...)
     
     Return the ceiling of x as an int.
     This is the smallest integral value >= x.
+
+
+Writing modules
+Writing Python modules is very simple. To create a module of your own, simply create a new .py file with the module name, and then import it using the Python file name (without the .py extension) using the import command.
+
+Writing packages
+Packages are name-spaces which contain multiple packages and modules themselves. They are simply directories, but with a twist.
+
+Each package in Python is a directory which MUST contain a special file called __init__.py. This file can be empty, and it indicates that the directory it contains is a Python package, so it can be imported the same way a module can be imported.
+
+If we create a directory called foo, which marks the package name, we can then create a module inside that package called bar. We also must not forget to add the __init__.py file inside the foo directory.
+
