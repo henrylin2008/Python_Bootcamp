5cb25d82e4c5c1b738881b06d9f90793986e4456
Henry Lin <henry.lin@appdirect.com>
2019-01-17 06:04:42 -0800

    oop

---
+++ OOP.py
@@ -0,0 +1,23 @@
+Object Oriented Programming
+Object Oriented Programming (OOP) tends to be one of the major obstacles for beginners when they are first starting to learn Python.
+
+There are many,many tutorials and lessons covering OOP so feel free to Google search other lessons, and I have also put some links to other useful tutorials online at the bottom of this Notebook.
+
+For this lesson we will construct our knowledge of OOP in Python by building on the following topics:
+
+Objects
+Using the class keyword
+Creating class attributes
+Creating methods in a class
+Learning about Inheritance
+Learning about Special Methods for classes
+Lets start the lesson by remembering about the Basic Python Objects. For example:
+
+In [1]:
+l = [1,2,3]
+Remember how we could call methods on a list?
+
+In [3]:
+l.count(2)
+Out[3]:
+1
