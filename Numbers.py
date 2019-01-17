59db325fe995a042dcf9b1070f490425fa565b31
Henry Lin <henry.lin@appdirect.com>
2019-01-16 22:03:33 -0800

    Numbers

---
+++ Numbers.py
@@ -0,0 +1,10 @@
+
+Numbers and more in Python!
+In this lecture, we will learn about numbers in Python and how to use them.
+
+We'll learn about the following topics:
+
+1.) Types of Numbers in Python
+2.) Basic Arithmetic
+3.) Differences between Python 2 vs 3 in division
+4.) Object Assignment in Python
