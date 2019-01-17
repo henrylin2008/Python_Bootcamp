e832dec1027b3aec0d4a7a21f51f7f5309fde4b6
Henry Lin <henry.lin@appdirect.com>
2019-01-17 06:09:27 -0800

    Print Formatting

---
+++ Print_Formatting.py
@@ -0,0 +1,8 @@
+Print Formatting
+In this lecture we will briefly cover the various ways to format your print statements. As you code more and more, you will probably want to have print statements that can take in a variable into a printed string statement.
+
+The most basic example of a print statement is:
+
+In [17]:
+print 'This is a string'
+This is a string
