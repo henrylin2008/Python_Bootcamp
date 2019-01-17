a4169f3bd9005d089f11eb69a6bdd176c87ffdfa
Henry Lin <henry.lin@appdirect.com>
2019-01-17 06:06:13 -0800

    oop

--- OOP.py
+++ OOP.py
@@ -162,3 +162,38 @@ print 'Area is: ',c.area()
 Radius is:  2
 Area is:  12.56
 Great! Notice how we used self. notation to reference attributes of the class within the method calls. Review how the code above works and try creating your own method
+
+Inheritance
+Inheritance is a way to form new classes using classes that have already been defined. The newly formed classes are called derived classes, the classes that we derive from are called base classes. Important benefits of inheritance are code reuse and reduction of complexity of a program. The derived classes (descendants) override or extend the functionality of base classes (ancestors).
+
+Lets see an example by incorporating our previous work on the Dog class:
+
+In [9]:
+class Animal(object):
+    def __init__(self):
+        print "Animal created"
+
+    def whoAmI(self):
+        print "Animal"
+
+    def eat(self):
+        print "Eating"
+
+
+class Dog(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+        print "Dog created"
+
+    def whoAmI(self):
+        print "Dog"
+
+    def bark(self):
+        print "Woof!"
+In [10]:
+d = Dog()
+Animal created
+Dog created
+In [25]:
+d.whoAmI()
+Dog
