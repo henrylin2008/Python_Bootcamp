835a64505649db131399f1f060ee43cce9579488
Henry Lin <henry.lin@appdirect.com>
2019-01-17 06:09:39 -0800

    Print Formatting

--- Print_Formatting.py
+++ Print_Formatting.py
@@ -6,3 +6,11 @@ The most basic example of a print statement is:
 In [17]:
 print 'This is a string'
 This is a string
+
+Strings
+You can use the %s to format strings into your print statements.
+
+In [4]:
+s = 'STRING'
+print 'Place another string with a mod and s: %s' %(s)
+Place another string with a mod and s: STRING
