1874c3b2203e146964091e6fbef4279376683dba
Henry Lin <henry.lin@appdirect.com>
2019-01-16 22:03:48 -0800

    Numbers

--- Numbers.py
+++ Numbers.py
@@ -8,3 +8,19 @@ We'll learn about the following topics:
 2.) Basic Arithmetic
 3.) Differences between Python 2 vs 3 in division
 4.) Object Assignment in Python
+
+
+Types of numbers
+Python has various "types" of numbers (numeric literals). We'll mainly focus on integers and floating point numbers.
+
+Integers are just whole numbers, positive or negative. For example: 2 and -2 are examples of integers.
+
+Floating point numbers in Python are notable because they have a decimal point in them, or use an exponential (e) to define the number. For example 2.0 and -2.1 are examples of floating point numbers. 4E2 (4 times 10 to the power of 2) is also an example of a floating point number in Python.
+
+Throughout this course we will be mainly working with integers or simple float number types.
+
+Here is a table of the two main types we will spend most of our time working with some examples:
+
+Examples	Number "Type"
+1,2,-5,1000	Integers
+1.2,-0.5,2e2,3E2	Floating-point numbers
