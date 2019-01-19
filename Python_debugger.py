71097548c95c153021690bacd47e9ef6e4939c4a
Henry Lin <henry.lin@appdirect.com>
2019-01-19 06:49:03 -0800

    PD

--- Python_debugger.py
+++ Python_debugger.py
@@ -43,3 +43,38 @@ pdb.set_trace()
 result2 = y+x
 print result2
 5
+
+--Return--
+> <ipython-input-4-0a2880872cf0>(11)<module>()->None
+-> pdb.set_trace()
+(Pdb) x
+[1, 3, 4]
+(Pdb) y
+2
+(Pdb) z
+3
+(Pdb) x+y
+*** TypeError: can only concatenate list (not "int") to list
+(Pdb) q
+---------------------------------------------------------------------------
+BdbQuit                                   Traceback (most recent call last)
+<ipython-input-4-0a2880872cf0> in <module>()
+      9 
+     10 # Set a trace using Python Debugger
+---> 11 pdb.set_trace()
+     12 
+     13 result2 = y+x
+
+//anaconda/lib/python2.7/bdb.pyc in trace_dispatch(self, frame, event, arg)
+     51             return self.dispatch_call(frame, arg)
+     52         if event == 'return':
+---> 53             return self.dispatch_return(frame, arg)
+     54         if event == 'exception':
+     55             return self.dispatch_exception(frame, arg)
+
+//anaconda/lib/python2.7/bdb.pyc in dispatch_return(self, frame, arg)
+     89             finally:
+     90                 self.frame_returning = None
+---> 91             if self.quitting: raise BdbQuit
+     92         return self.trace_dispatch
+     93 
