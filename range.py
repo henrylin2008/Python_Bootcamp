01e948db1387fed133f384a779997d31817b27b4
Henry Lin <henry.lin@appdirect.com>
2019-01-19 06:50:23 -0800

    range

--- range.py
+++ range.py
@@ -30,3 +30,11 @@ x
 Out[5]:
 [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
 Awesome! Well thats it...or is it?
+
+You might have been wondering, what happens if I want to use a huge range of numbers? Can my computer store that all in memory?
+
+Great thinking! This is a dilemma that can be solve with the use of a generator. For a simplified explanation: A generator allows the generation of generated objects that are provided at that instance but does not store every instance generated into memory.
+
+This means a generator would not create a list to generate like range() does, but instead provide a one time generation of the numbers in that range. Python 2 has a built-in range generator called xrange(). It is recommended to use xrange() for for loops in Python 2.
+
+The good news is in Python 3, range() behaves as a generator and you don't need to worry about it. Let's see a quick example with xrange()
