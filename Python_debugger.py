6e0ad29be0b965785faa91f360986b0e2804ca85
Henry Lin <henry.lin@appdirect.com>
2019-01-19 06:48:12 -0800

    PD

---
+++ Python_debugger.py
@@ -0,0 +1,4 @@
+Python Debugger
+You've probably used a variety of print statements to try to find errors in your code. A better way of doing this is by using Python's built-in debugger module (pdb). The pdb module implements an interactive debugging environment for Python programs. It includes features to let you pause your program, look at the values of variables, and watch program execution step-by-step, so you can understand what your program actually does and find bugs in the logic.
+
+This is a bit difficult to show since it requires creating an error on purpose, but hopefully this simple example illustrates the power of the pdb module. Note: Keep in mind it would be pretty unusual to use pdb in an iPython Notebook setting.
