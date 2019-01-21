cbad41ef924c91d7cc91cc321f79d7abd880df58
Henry Lin <henry.lin@appdirect.com>
2019-01-20 21:44:09 -0800

    Regular Expression

--- Regular_expressions.py
+++ Regular_expressions.py
@@ -5,3 +5,26 @@ Regular expressions are text matching patterns described with a formal syntax. Y
 If you're familiar with Perl, you'll notice that the syntax for regular expressions are very similar in Python. We will be using the re module with Python for this lecture.
 
 Let's get started!
+
+Searching for Patterns in Text
+One of the most common uses for the re module is for finding patterns in text. Let's do a quick example of using the search method in the re module to find some text:
+
+In [8]:
+import re
+
+# List of patterns to search for
+patterns = [ 'term1', 'term2' ]
+
+# Text to parse
+text = 'This is a string with term1, but it does not have the other term.'
+
+for pattern in patterns:
+    print 'Searching for "%s" in: \n"%s"' % (pattern, text),
+    
+    #Check for match
+    if re.search(pattern,  text):
+        print '\n'
+        print 'Match was found. \n'
+    else:
+        print '\n'
+        print 'No Match was found.\n'
