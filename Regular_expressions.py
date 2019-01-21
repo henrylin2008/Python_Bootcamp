9ef7978d01b96982e5cc476d2b3bed3f7aaf7983
Henry Lin <henry.lin@appdirect.com>
2019-01-20 21:46:14 -0800

    Regular Expression

--- Regular_expressions.py
+++ Regular_expressions.py
@@ -114,3 +114,26 @@ def multi_re_find(patterns,phrase):
         print 'Searching the phrase using the re check: %r' %pattern
         print re.findall(pattern,phrase)
         print '\n'
+
+Repetition Syntax
+There are five ways to express repetition in a pattern:
+
+1.) A pattern followed by the meta-character * is repeated zero or more times. 
+2.) Replace the * with + and the pattern must appear at least once. 
+3.) Using ? means the pattern appears zero or one time. 
+4.) For a specific number of occurrences, use {m} after the pattern, where m is replaced with the number of times         the pattern should repeat. 
+5.) Use {m,n} where m is the minimum number of repetitions and n is the maximum. Leaving out n ({m,}) means the           value appears at least m times, with no maximum.
+
+Now we will see an example of each of these using our multi_re_find function:
+
+In [48]:
+test_phrase = 'sdsd..sssddd...sdddsddd...dsds...dsssss...sdddd'
+
+test_patterns = [ 'sd*',     # s followed by zero or more d's
+                'sd+',          # s followed by one or more d's
+                'sd?',          # s followed by zero or one d's
+                'sd{3}',        # s followed by three d's
+                'sd{2,3}',      # s followed by two to three d's
+                ]
+
+multi_re_find(test_patterns,test_phrase)
