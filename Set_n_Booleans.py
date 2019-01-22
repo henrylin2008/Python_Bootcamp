b315fa928e97b97a1618249455364cd96ce1591e
Henry Lin <henry.lin@appdirect.com>
2019-01-22 06:06:10 -0800

    Set & Booleans

---
+++ Set_n_Booleans.py
@@ -0,0 +1,16 @@
+Set and Booleans
+There are two other object types in Python that we should quickly cover. Sets and Booleans.
+
+Sets
+Sets are an unordered collection of unique elements. We can construct them by using the set() function. Let's go ahead and make a set to see how it works
+
+In [1]:
+x = set()
+In [3]:
+# We add to sets with the add() method
+x.add(1)
+In [4]:
+#Show
+x
+Out[4]:
+{1}
