24388ef592a8637108818f24905fff3115da86e0
Henry Lin <henry.lin@appdirect.com>
2019-01-22 06:09:45 -0800

    SAS

--- Statements_assessment_solutions.py
+++ Statements_assessment_solutions.py
@@ -26,3 +26,22 @@ In [1]:
 [x for x in range(1,50) if x%3 == 0]
 Out[1]:
 [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48]
+
+
+Go through the string below and if the length of a word is even print "even!"
+
+In [2]:
+st = 'Print every word in this sentence that has an even number of letters'
+In [4]:
+for word in st.split():
+    if len(word)%2 == 0:
+        print word+" <-- has an even length!"
+word <-- has an even length!
+in <-- has an even length!
+this <-- has an even length!
+sentence <-- has an even length!
+that <-- has an even length!
+an <-- has an even length!
+even <-- has an even length!
+number <-- has an even length!
+of <-- has an even length!
