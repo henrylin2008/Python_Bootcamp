0a3dd7b9198eba8897bf2dfa88f385da5cb5df4b
Henry Lin <henry.lin@appdirect.com>
2019-01-22 06:09:33 -0800

    SAS

--- Statements_assessment_solutions.py
+++ Statements_assessment_solutions.py
@@ -18,3 +18,11 @@ In [1]:
 range(0,11,2)
 Out[1]:
 [0, 2, 4, 6, 8, 10]
+
+
+Use List comprehension to create a list of all numbers between 1 and 50 that are divisible by 3.
+
+In [1]:
+[x for x in range(1,50) if x%3 == 0]
+Out[1]:
+[3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48]
