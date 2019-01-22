f954d7d9d216df5c2e4f0cd9f716a230ca16474e
Henry Lin <henry.lin@appdirect.com>
2019-01-22 06:09:58 -0800

    SAS

--- Statements_assessment_solutions.py
+++ Statements_assessment_solutions.py
@@ -45,3 +45,16 @@ an <-- has an even length!
 even <-- has an even length!
 number <-- has an even length!
 of <-- has an even length!
+
+Write a program that prints the integers from 1 to 100. But for multiples of three print "Fizz" instead of the number, and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+
+In [ ]:
+for num in xrange(1,101):
+    if num % 5 == 0 and num % 3 == 0:
+        print "FizzBuzz"
+    elif num % 3 == 0:
+        print "Fizz"
+    elif num % 5 == 0:
+        print "Buzz"
+    else:
+        print num
