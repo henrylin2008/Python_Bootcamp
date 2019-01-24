47a04464a9568f413fe59be66c4626a2330a76cc
Henry Lin <henry.lin@appdirect.com>
2019-01-23 21:30:41 -0800

    SIO

---
+++ StringIO.py
@@ -0,0 +1,6 @@
+The StringIO module implements an in-memory file like object. This object can then be used as input or output to most functions that would expect a standard file object.
+
+The best way to show this is by example:
+
+In [1]:
+import StringIO
