9eba8f48b1512565d9751d0e12cd4d55e8d47163
Henry Lin <henry.lin@appdirect.com>
2019-01-25 05:05:35 -0800

    Strings

--- Strings.py
+++ Strings.py
@@ -1,3 +1,16 @@
 Strings
-Strings are used in Python to record text information, such as name. Strings in Python are actually a sequence, which basically means Python keeps track of every element in the string as a sequence. For example, Python understands the string "hello' to be a sequence of letters in a specific order. This means we will be able to use indexing to grab particular letters (like the first letter, or the last letter).
+Strings are used in Python to record text information, such as name. Strings in Python are actually a sequence, which basically means Python keeps track of every element in the 
+string as a sequence. For example, Python understands the string "hello' to be a sequence of letters in a specific order. This means we will be able to use indexing to grab 
+particular letters (like the first letter, or the last letter).
 
+This idea of a sequence is an important one in Python and we will touch upon it later on in the future.
+
+In this lecture we'll learn about the following:
+
+1.) Creating Strings
+2.) Printing Strings
+3.) Differences in Printing in Python 2 vs 3
+4.) String Indexing and Slicing
+5.) String Properties
+6.) String Methods
+7.) Print Formatting
