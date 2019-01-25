1062590fcbbb31f5bd2cfbba138d1c959bd32c42
Henry Lin <henry.lin@appdirect.com>
2019-01-25 05:05:05 -0800

    Strings

---
+++ Strings.py
@@ -0,0 +1,3 @@
+Strings
+Strings are used in Python to record text information, such as name. Strings in Python are actually a sequence, which basically means Python keeps track of every element in the string as a sequence. For example, Python understands the string "hello' to be a sequence of letters in a specific order. This means we will be able to use indexing to grab particular letters (like the first letter, or the last letter).
+
